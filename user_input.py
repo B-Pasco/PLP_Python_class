40a1459c371a80cf64553afc48728c22d57816c1
B-Pasco <mugishapasco02@gmail.com>
2024-10-30 21:19:51 +0200

    python

---
+++ user_input.py
@@ -0,0 +1,6 @@
+# Taking user input
+name = input("What is your full name?\n")
+age = int(input("How old are you today?\n"))
+location = input("Where do you currently live?\n")
+
+print(f"Hello {name}, you are {age} years old and you live in {location}.")
