c3acd1cd8f91f54665d648aade7825cb71eb3802
B-Pasco <mugishapasco02@gmail.com>
2024-11-10 16:11:43 +0200

    list

--- week_2.py
+++ week_2.py
@@ -10,7 +10,18 @@ my_list.append(40)
 # Insert the value 15 at the second position in the list.
 my_list.insert(1, 15)
 
+# Extend my_list with another list: [50, 60, 70].
 another_list = [50, 60, 70]
 my_list.extend(another_list)
 
+# Remove the last element from my_list.
+my_list.pop()
+
+# Sort my_list in ascending order.
+my_list.sort()
+
+# Find and print the index of the value 30 in my_list.
+print(my_list.index(30))
+
+# let print our list
 print(my_list)
