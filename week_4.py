2ed471c63547aacaa92dad5f78af052a88552821
B-Pasco <mugishapasco02@gmail.com>
2024-12-04 22:01:44 +0200

    error handling

---
+++ week_4.py
@@ -0,0 +1,7 @@
+"""
+- File Read & Write Challenge: Create a program that reads a file and writes a modified version to a new file.
+- Error Handling Lab: Ask the user for a filename and handle errors if it doesn’t exist or can’t be read.
+
+Learning Outcomes:
+I will be skilled in managing files efficiently in Python, ensuring error-free code that gracefully handles unexpected issues. Mastering files and exception handling will allow you to build strong, robust applications!
+"""
