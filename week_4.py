c2f2df14f28bc988fed7285ff58f542e3c439ee2
B-Pasco <mugishapasco02@gmail.com>
2024-12-04 22:14:11 +0200

    error handling

--- week_4.py
+++ week_4.py
@@ -5,3 +5,16 @@
 Learning Outcomes:
 I will be skilled in managing files efficiently in Python, ensuring error-free code that gracefully handles unexpected issues. Mastering files and exception handling will allow you to build strong, robust applications!
 """
+try:
+    # Prompt the user to enter a filename.
+    file_name = input("Enter a name of your file to read: ")
+    
+    # Open the file in read mode.
+    with open(file_name, 'r') as file:
+        data = file.read()
+        
+        # Print the contents of the file.
+        print(data)
+
+except FileNotFoundError:
+    print("File not found. Please check the file name and try again.")
